3fdab2ad4cdc897a4616dfb4b2b4f232b5b9be10
JosueChan-2022047 <jchan-2022047@kinal.edu.gt>
2026-04-15 02:26:34 -0600

    fix: Se corrigieron rutas, metodos y especificaciones

--- demo (2)/demo/src/main/java/com/example/demo/Controller/LoginController.java
+++ demo (2)/demo/src/main/java/com/example/demo/Controller/LoginController.java
@@ -1,12 +1,20 @@
 package com.example.demo.Controller;
 
+import com.example.demo.entity.Usuarios;
+import com.example.demo.service.UsuarioService;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController {
+
+    @Autowired
+    private UsuarioService usuarioService;
 
     @GetMapping("/login")
     public String login() {
@@ -14,13 +22,45 @@ public class LoginController {
     }
 
     @PostMapping("/login")
-    public String validar(@RequestParam String usuario,
-                          @RequestParam String password) {
+    public String autenticar(@RequestParam("usuario") String userForm,
+                             @RequestParam("password") String passForm,
+                             HttpSession session) {
 
-        if (usuario.equals("admin") && password.equals("1234")) {
+
+        Usuarios user = (Usuarios) usuarioService.buscarPorUsername(userForm.trim());
+
+        if (user != null && user.getPassword().trim().equals(passForm.trim())) {
+            session.setAttribute("usuarioLogueado", user);
             return "redirect:/home";
+        } else {
+            return "redirect:/login?error=true";
         }
+    }
 
-        return "redirect:/login?error=true";
+    @GetMapping("/logout")
+    public String logout(HttpSession session) {
+        session.invalidate();
+        return "redirect:/login?logout=true";
+    }
+
+    @PostMapping("/registro")
+    public String registrarUsuario(@ModelAttribute Usuarios nuevoUsuario) {
+        try {
+
+            nuevoUsuario.setRol("USER");
+            nuevoUsuario.setEstado(1);
+
+
+            usuarioService.saveUsuario(nuevoUsuario);
+
+            return "redirect:/login?registrado=true";
+        } catch (Exception e) {
+            return "redirect:/login?errorRegistro=true";
+        }
+    }
+
+    @GetMapping("/")
+    public String index() {
+        return "redirect:/login";
     }
 }
