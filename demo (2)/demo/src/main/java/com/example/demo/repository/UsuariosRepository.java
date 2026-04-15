92ce9c9874186c087708f9e27c18cc8805f7297d
JosueChan-2022047 <jchan-2022047@kinal.edu.gt>
2026-04-15 02:39:56 -0600

    fix: Corrección de las rutas y el envío de JSON

--- demo (2)/demo/src/main/java/com/example/demo/repository/UsuariosRepository.java
+++ demo (2)/demo/src/main/java/com/example/demo/repository/UsuariosRepository.java
@@ -2,10 +2,11 @@ package com.example.demo.repository;
 
 import com.example.demo.entity.Usuarios;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
-@Repository
 public interface UsuariosRepository extends JpaRepository<Usuarios, Integer> {
 
-
+    @Query("SELECT u FROM Usuarios u WHERE u.username = :username")
+    Usuarios findByUsername(@Param("username") String username);
 }
