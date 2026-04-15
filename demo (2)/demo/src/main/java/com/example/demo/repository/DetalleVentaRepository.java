209163adabf32d023fc5bff4e7208933f3acd712
JosueChan-2022047 <jchan-2022047@kinal.edu.gt>
2026-04-15 02:39:24 -0600

    fix: Corrección de las rutas y el envío de JSON

--- demo (2)/demo/src/main/java/com/example/demo/repository/DetalleVentaRepository.java
+++ demo (2)/demo/src/main/java/com/example/demo/repository/DetalleVentaRepository.java
@@ -2,12 +2,14 @@ package com.example.demo.repository;
 
 import com.example.demo.entity.DetalleVenta;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-
 import java.util.List;
 
 @Repository
 public interface DetalleVentaRepository extends JpaRepository<DetalleVenta, Integer> {
 
-
+    @Query("SELECT d FROM DetalleVenta d WHERE d.venta.codigo_venta = :cod")
+    List<DetalleVenta> findByCodigoVenta(@Param("cod") Integer cod);
 }
